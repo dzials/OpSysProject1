6d87c0c3f3f24f3e345aeebb953706c049dbd73f
U-NEEMAYW541\neemay <yardenn1@gmail.com>
2017-03-01 20:36:52 -0500

    Finished FCFS with statistics

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <deque>
+#include <fstream>
+#include <iomanip>
 #include "Parse.h"
 
 using namespace std;
@@ -9,7 +11,7 @@ using namespace std;
 */
 
 // GLOBAL VARIABLES
-// 
+//
 // map of processes
 map<string,Process> processes;
 
@@ -19,15 +21,17 @@ deque<Process> readyQueue;
 
 
 // Number of processes
-int n = 0; 
+int n = 0;
 // Number of processors (cores)
-int m = 1; 
+int m = 1;
 // Time for context switch
-int t_cs = 6; 
+int t_cs = 6;
 // Round-robin time slice
-int t_slice = 94; 
+int t_slice = 94;
 // Time counter
 int t = 0;
+//Number of context switches
+int cs = 0;
 
 //For debugging Parser.h
 void print_processes_map() {
@@ -40,12 +44,13 @@ void print_processes_map() {
 void reset() {
     readyQueue = deque<Process>();
     t = 0;
+    cs = 0;
 }
 
 string printQueue() {
     string build = "[Q";
     if (readyQueue.size() == 0) {
-       
+
         return build += " <empty>]";
     }
     for (deque<Process>::iterator itr = readyQueue.begin(); itr != readyQueue.end(); itr++) {
@@ -56,19 +61,57 @@ string printQueue() {
 
 float calculate_waits() {
     float sum = 0;
+    float bursts = 0;
     for (map<string, Process>::iterator itr = processes.begin(); itr != processes.end(); ++itr)
     {
-        sum += ((itr->second.total_wait));
+        sum += (itr->second.total_wait);
+        bursts += itr->second.total_bursts;
     }
-    return sum / n;
+    return sum / bursts;
 }
 
-
+float calculate_avg_CPU() {
+    float sum = 0;
+    float bursts = 0;
+    for (map<string, Process>::iterator itr = processes.begin(); itr != processes.end(); ++itr)
+    {
+        sum += ((itr->second.burst_time) * itr->second.total_bursts);
+        bursts += (itr->second.total_bursts);
+    }
+    return sum / bursts;
+}
 
 float calculate_turnaround() {
-    return 0.0;
+
+    return calculate_waits() + calculate_avg_CPU() + (t_cs/2);
 }
 
+int calculate_pre() {
+    int total = 0;
+    for (map<string, Process>::iterator itr = processes.begin(); itr != processes.end(); ++itr)
+    {
+        total += (itr->second.preemption_cnt);
+    }
+    return total;
+}
+
+void print_statistics(const char* filename, int x) {
+    ofstream output;
+    output.open(filename);
+    if(x == 1)
+        output << "Algorithm FCFS" << endl;
+    else if(x == 2)
+        output << "Algorithm SRT" << endl;
+    else
+        output << "Algorithm RR" << endl;
+
+    output << "-- average CPU burst time: " << setprecision(2) << fixed << calculate_avg_CPU() << " ms" << endl;
+    output << "-- average wait time: " << setprecision(2) << fixed << calculate_waits() << " ms" << endl;
+    output << "-- average CPU burst time: " << setprecision(2) << fixed <<  calculate_turnaround() << " ms" << endl;
+    output << "-- total number of context siwtches: " << cs << endl;
+    output << "-- total number of preemptions: " << calculate_pre() << endl;
+    output.close();
+}
 void FCFS() {
     int term_cnt = 0;
     int current_process_burst_time = 0;
@@ -86,11 +129,8 @@ void FCFS() {
         map<string, Process>::iterator itr = processes.begin();
         while (itr->second.arrival_time <= t &&  itr != processes.end()) {
             if (itr->second.arrival_time == t) {
+                itr->second.ready_start = t + (t_cs/2);
                 readyQueue.push_back(itr->second);
-                itr->second.ready_start = t;
-                if(running == NULL) {
-                    itr->second.ready_start = t + (t_cs/2);
-                }
                 printf("time %dms: Process %s arrived and added to ready queue %s\n", t, itr->second.p_name.c_str(), (printQueue()).c_str());
                 fflush (stdout);
             }
@@ -100,30 +140,16 @@ void FCFS() {
 
         // Handle blocking processes
         for (map<string, Process>::iterator itr2 = processes.begin(); itr2 != processes.end(); ++itr2) {
-            /*
-            if (t == 605) {
-                cout << printQueue() << " " << itr2->second.end_blocking_time << endl;
-            }
-            if (t == 606) {
-                cout << printQueue() << " " << itr2->second.end_blocking_time << endl;
-            }
-            if (t == 607) {
-                cout << printQueue() << " " << itr2->second.end_blocking_time << endl;
-            }
-            */
             if (t == itr2->second.end_blocking_time) {
                 itr2->second.end_blocking_time = -1;
                 readyQueue.push_back(itr2->second);
-                itr2->second.ready_start = t;
+                itr2->second.ready_start = t + (t_cs/2);
                 printf("time %dms: Process %s completed I/O; added to ready queue %s\n", t, itr2->second.p_name.c_str(), printQueue().c_str());
                 fflush(stdout);
                 if (running == NULL) {
                     context_switch_timer = t_cs / 2;
-                    itr2->second.ready_start = t + t_cs / 2;
-
                 }
             }
-            //cout << "time is: " << t << " " << itr2->second.p_name << " " << printQueue() << endl;
         }
 
 
@@ -134,10 +160,10 @@ void FCFS() {
         } else if (current_process_burst_time == 0 && context_switch_timer == 0){
             // Switch to new process
             if (!readyQueue.empty()) {
-
+                cs++;
                 running = &readyQueue.front();
                 readyQueue.pop_front();
-                processes.find(running->p_name)->second.total_wait += t - processes.find(running->p_name)->second.ready_start;
+                processes.find(running->p_name)->second.total_wait += (t - (processes.find(running->p_name)->second.ready_start));
 
                 printf("time %dms: Process %s started using the CPU %s\n", t, running->p_name.c_str(), printQueue().c_str());
                 fflush(stdout);
@@ -156,7 +182,7 @@ void FCFS() {
                     if (term_cnt == n) {
                         t += (t_cs / 2) - 1;
                     }
-                    
+
                 } else {
                     if (processes.find(running->p_name)->second.num_burst == 1) {
                         printf("time %dms: Process %s completed a CPU burst; %d burst to go %s\n", t, running->p_name.c_str(), processes.find(running->p_name)->second.num_burst,  printQueue().c_str());
@@ -172,8 +198,6 @@ void FCFS() {
                 running = NULL;
             }
         }
-
-
         t++;
     }
     printf("time %dms: Simulator ended for FCFS\n", t);
@@ -193,8 +217,7 @@ int main(int argc, char const *argv[]) {
 
     reset();
     FCFS();
-
-    cout << calculate_waits() << endl;
+    print_statistics(argv[2], 1);
 
     return 0;
 }
